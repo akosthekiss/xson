5f458f98861075a72a1a9356921d5c09c479afed
Akos Kiss (via Travis CI) <akiss@inf.u-szeged.hu>
2020-04-04 15:06:35 +0000

    [Documentation] (c4637eff3ceb0671586d14502c3465a711eca167)

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.2.dev8+g73e5f2e',
+    VERSION: '1.0.2.dev8+gc4637ef',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
