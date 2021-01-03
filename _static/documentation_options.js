9c38c2e7547a6fd244f807c2f93b247cd3317d04
akosthekiss <akosthekiss@users.noreply.github.com>
2021-01-03 13:02:58 +0000

    (ee7509710040788d8d005ec288758b4dab1bc613) Deploy Sphinx-generated documentation to GitHub Pages via Actions
    TODO before landing:
    - Change pages.yml:on.push.branches from * to master
    - Change shields.io badge URL and target URL from akosthekiss to
      renatahodovan

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g4bf7449',
+    VERSION: '0.1.dev1+gee75097',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
